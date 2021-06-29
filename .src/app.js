140b72ca0637a8e35f86b121d48d74c2210ec95c
Java Monk <javamonk.coder@gmail.com>
2021-06-29 17:16:26 +0530

    added singlenote logic

--- .src/app.js
+++ .src/app.js
@@ -1,39 +1,33 @@
-console.log("Js File");
-
-// adding a Note
-
-let submitNote = document.getElementById("submitNote");
-submitNote.addEventListener("click", function (e) {
-  e.preventDefault();
-  let noteTitle = document.getElementById("noteTitle");
-  let noteTxtArea = document.getElementById("noteTxtArea");
-
-  if (noteTitle.value || noteTxtArea.value == null) {
-    notesArr = [];
-  } else {
-      notesArr = JSON.parse(noteTitle.value,noteTxtArea.value);
-  }
-
-  notesArr.push(noteTitle.value, noteTxtArea.value);
-  localStorage.setItem('notes',JSON.stringify(notesArr));
-    showNotes();
-});
-
-function showNotes() {
-  let noteTitle = localStorage.getItem("notes")[3];
-  let noteTxtArea = localStorage.getItem("notes")[1];
-
-  let cardNote = document.getElementById("notesRender");
-
-  let elemCardHtml = ` <div class="col-lg-4 my-3 col-md-6 col-sm-12">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">${noteTitle}</h5>
-        <p class="card-text">${noteTxtArea}</p>
-        <button class="btn btn-outline-danger" style="width: 10rem;">Delete&nbsp;
-          <i class="far fa-trash-alt"></i></button>
-      </div>
-    </div>`;
-
-  cardNote.innerHTML = elemCardHtml;
+if (true) {
+  var notesDisplay = document.getElementsByClassName("displayEmpty")[0];
+  notesDisplay.innerHTML =
+    "Nothing To Show Here :( <br> Create a Note To See Here";
 }
+
+let noteObj;
+var noteTitle = document.getElementById('noteTitle');
+var noteTxtArea = document.getElementById('noteTxtArea');
+
+function takeNote() {
+  notesDisplay.classList.remove("displayEmpty");
+  notesDisplay.innerHTML = "";
+
+  let checkNotes =  localStorage.getItem('notes');
+
+  if(checkNotes == null) {
+  noteObj = {title: `${noteTitle.value}` , content :`${noteTxtArea.value}`};
+  let myJson = JSON.stringify(noteObj);
+  localStorage.setItem('notes',myJson);
+  } else {
+    let myJson = JSON.parse(checkNotes);
+    console.log(myJson);
+  }
+}
+
+function displayNote() {
+  let noteDiv;
+}
+
+function deleteNote() {}
+
+function markImp() {}
